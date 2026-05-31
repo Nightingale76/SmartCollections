cefdd80ffdf93a3bc642032ba37401431b755243
lenovo <1577215020@qq.com>
2026-05-31 09:35:33 +0800

    没有改成功变成没标签了

--- qwen-config.js
+++ qwen-config.js
@@ -2,7 +2,7 @@
 // Get API key: https://bailian.console.aliyun.com/
 
 const DASHSCOPE_API_KEY = 'sk-64a8de4019a6427e83bc90c6ff87831c';
-const QWEN_MODEL = 'qwen-plus';
+const QWEN_MODEL = 'qwen3.6-plus';
 const QWEN_BASE_URL = 'https://dashscope.aliyuncs.com/compatible-mode/v1';
 
 window.SMART_COLLECTIONS_AI_CONFIG = {
